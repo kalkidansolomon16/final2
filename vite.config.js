58d1337022a1175fabbe31623e7f2f0a16177d77
complatform <kalkidansol1623@gmail.com>
2025-02-09 07:49:38 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
+// Define the Vite configuration
 export default defineConfig({
   plugins: [
     vue(),
@@ -12,7 +11,11 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  // Set the public path based on the environment
+  base: process.env.NODE_ENV === 'production'
+    ? '/final2/'
+    : '/',
+});
